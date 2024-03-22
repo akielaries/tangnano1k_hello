964db50af668d5c59bf126235f518848351e3232
akielaries <akiel@akiel.org>
2024-03-21 21:23:57 -0700

    changing LED flash speed

--- blinky.v
+++ blinky.v
@@ -10,11 +10,11 @@ module top (
 
 // Register to hold the current count value
 //reg [25:0] ctr_q;
-reg [30:0] ctr_q;
+reg [28:0] ctr_q;
 
 // Wire to calculate the next count value
 //wire [25:0] ctr_d;
-wire [30:0] ctr_d;
+wire [28:0] ctr_d;
 
 // Sequential code (flip-flop)
 always @(posedge clk) begin
@@ -32,6 +32,6 @@ assign ctr_d = ctr_q + 1'b1;
 // Drive LED outputs based on the most significant bit of the count value
 // This effectively divides the clock frequency by 2^(`LEDS_NR), causing LEDs to blink at a slower rate
 //assign led = ctr_q[25:25-(`LEDS_NR - 1)];
-assign led = ctr_q[30:30-(`LEDS_NR - 1)];
+assign led = ctr_q[28:28-(`LEDS_NR - 1)];
 
 endmodule
